8e8c15bd4831ab77c9d2755867cf6087e1a0c53c
Diego Véliz <u20211b564@upc.edu.pe>
2025-11-12 19:51:30 -0500

    feat:"glucomente/domain"

---
+++ DevsPros.Diabelife.Platform.API/Glucometer/domain/model/GlucoseMeasurement.cs
@@ -0,0 +1,33 @@
+﻿using DevsPros.Diabelife.Platform.API.Shared.Domain.Model;
+
+namespace DevsPros.Diabelife.Platform.API.Glucometer.Domain.Model;
+
+public class GlucoseMeasurement : BaseEntity
+{
+    public double Value { get; set; }
+    public string Unit { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public string Trend { get; set; } = string.Empty;
+    public DateTime MeasurementDate { get; set; }
+
+    public GlucoseMeasurement() { }
+
+    public GlucoseMeasurement(double value, string unit, string status, string trend, DateTime measurementDate)
+    {
+        Value = value;
+        Unit = unit;
+        Status = status;
+        Trend = trend;
+        MeasurementDate = measurementDate;
+    }
+
+    public void Update(double value, string unit, string status, string trend, DateTime measurementDate)
+    {
+        Value = value;
+        Unit = unit;
+        Status = status;
+        Trend = trend;
+        MeasurementDate = measurementDate;
+        UpdatedAt = DateTime.UtcNow;
+    }
+}
