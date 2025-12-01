0c473872ef520b9dd960a7b60527c87ee89a4e6b
Gabriel Mamani <u202220659@upc.edu.pe>
2025-11-30 19:02:27 -0500

    update cors

--- DevsPros.Diabelife.Platform.API/Program.cs
+++ DevsPros.Diabelife.Platform.API/Program.cs
@@ -98,7 +98,7 @@ builder.Services.AddCors(options =>
         policy.WithOrigins(
                 "http://localhost:5173",
                 "https://diabelife-frontend.netlify.app",
-                "https://glistening-beijinho-db43a1.netlify.app"
+                "https://diabelife-application.netlify.app"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
